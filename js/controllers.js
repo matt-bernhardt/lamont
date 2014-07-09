6b308f91e1ec2f45f3c679f94adfdf5a6a94bec0
Matthew Bernhardt <mjbernha@mit.edu>
2014-07-09 01:04:36 -0400

    More details
    Switching over to live API (separate repo)
    Simplifying data displayed on players table (player info only, not
    performance/stats)
    A bit more debugging on player detail view (still not working)

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,19 @@
 var playerControllers = angular.module('playerControllers', []);
 
 playerControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-	$http.get('api/players.json').success(function(data) {
+	$http.get('/api/players.php').success(function(data) {
 		$scope.players = data;
 		$scope.playerOrder = 'name';
 	});
 }]);
 
 playerControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-	$http.get('api/players.json').success(function(data) {
+	console.log('Looking for ' + $routeParams.playerId);
+	$http.get('/api/players.json').success(function(data) {
 		$scope.players = data;
 		$scope.whichItem = $routeParams.playerId;
+		console.log('Looking for '+$scope.whichItem);
+	}).failure(function(data) {
+		console.log('Failure');
 	});
 }]);
