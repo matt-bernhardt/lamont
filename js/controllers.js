4808d9440002d6714ac7c5ad1605817f35bd9a90
Matthew Bernhardt <mjbernha@mit.edu>
2014-07-10 00:29:02 -0400

    Player Detail Page
    Adding player detail page, including call to separate API with single
    player data

--- js/controllers.js
+++ js/controllers.js
@@ -7,13 +7,14 @@ playerControllers.controller('ListController', ['$scope', '$http', function($sco
 	});
 }]);
 
-playerControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-	console.log('Looking for ' + $routeParams.playerId);
-	$http.get('/api/players.json').success(function(data) {
-		$scope.players = data;
-		$scope.whichItem = $routeParams.playerId;
-		console.log('Looking for '+$scope.whichItem);
-	}).failure(function(data) {
-		console.log('Failure');
+playerControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', '$log', function($scope, $http, $routeParams, $log) {
+	$scope.playerId = $routeParams.playerId;
+	$log.log('Looking for ' + $scope.playerId);
+	$http.get('/api/player.php?term=' + $scope.playerId).success(function(data) {
+		$log.log(data);
+		$scope.player = data;
+	}).error(function(data) {
+		$log.log('Failure receiving data from API');
 	});
+
 }]);
