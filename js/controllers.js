0d840b27d23040c42fda641d010f00969390d7f7
Matthew Bernhardt <mjbernha@mit.edu>
2014-07-15 00:24:47 -0400

    Implementing faceted filtering
    Still very rough, with lots of debugging and need for refactoring - but
    multi-dimensional filtering is now working

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,11 @@
 var playerControllers = angular.module('playerControllers', []);
 
 playerControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $scope.search = {
+        "PlayerName" : "",
+        "Position" : {},
+        "Citizenship" : {}
+    };
 	$http.get('/api/players.php').success(function(data) {
 		$scope.players = data;
 		$scope.playerOrder = 'name';
@@ -20,21 +25,64 @@ playerControllers.controller('DetailsController', ['$scope', '$http', '$routePar
 
 myApp.filter('playerFilter', function() {
     return function( list, searchobj ) {
+    	console.log("Filtering...");
+        console.log(searchobj);
+        console.log("Length: " + searchobj.size);
         return list.filter( function( item ) {
-            var passes_filters = false,
-                any_filter_set = false;
 
-            for ( Position in searchobj.Position ) {
+            console.log('-----------');
+        	console.log(item.PlayerName + ' - ' + item.Position + ' - ' + item.Citizenship);
+
+            // Check for filters set
+            var any_filter_set = false;
+            if ( searchobj.hasOwnProperty('PlayerName') && searchobj.PlayerName != "" ) {
+                any_filter_set = true;
+            }
+            for ( Position in searchobj.Position) {
                 any_filter_set = any_filter_set || searchobj.Position[ Position ];
+            }
+            for ( Citizenship in searchobj.Citizenship) {
+                any_filter_set = any_filter_set || searchobj.Citizenship[ Citizenship ];
+            }
+            // If any_filter_set is still false, just pass everything through
+            if ( !any_filter_set ) { return !any_filter_set; }
+
+            // Still here? Do the filters pass?
+            var any_value_set = false;
+            var passes_filters = false;
+
+            console.log('Player Name: ' + searchobj.PlayerName);
+            if ( searchobj.PlayerName != "" && item.PlayerName.indexOf(searchobj.PlayerName) === -1 ) {
+                console.log('  Failed name filter');
+                return false;
+            }
+
+            console.log('Position:');
+            console.log(searchobj.Position);
+            for ( Position in searchobj.Position ) {
+                any_value_set = any_value_set || searchobj.Position[Position];
                 passes_filters = passes_filters || (searchobj.Position[ Position ] && item.Position == Position);
             }
-
-            for ( Citizenship in searchobj.Citizenship ) {
-                any_filter_set = any_filter_set || searchobj.Citizenship[ Citizenship ];
-                passes_filters = passes_filters || (searchobj.Citizenship[ Citizenship ] && item.Citizenship == Citizenship);
+            if( any_value_set && !passes_filters ) {
+                console.log('  Failed position filter');
+                return false;
             }
 
-            return !any_filter_set || passes_filters;
+            console.log('Citizenship:');
+            console.log(searchobj.Citizenship);
+            any_value_set = false;
+            passes_filters = false;
+            for ( Citizenship in searchobj.Citizenship ) {
+                console.log(item);
+                any_value_set = any_value_set || searchobj.Citizenship[ Citizenship ];
+                passes_filters = passes_filters || (searchobj.Citizenship[ Citizenship ] && item.Citizenship.replace(/\s/g,'') == Citizenship);
+            }
+            if( any_value_set && !passes_filters ) {
+                console.log('  Failed citizenship filter');
+                return false;
+            }
+            console.log('Passes filters: ' + passes_filters);
+            return true;
         } );
     };
 });
